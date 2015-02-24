e90c0758593f95053ba9190683cf825256fa1403
Michael Heuer <heuermh@acm.org>
2015-02-24 16:17:48 -0600

    update copyright years to include 2015

--- src/main/java/com/github/heuermh/adam/plugins/JavaADAMPlugin.java
+++ src/main/java/com/github/heuermh/adam/plugins/JavaADAMPlugin.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 held jointly by the individual authors.
+ * Copyright 2014-2015 held jointly by the individual authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
